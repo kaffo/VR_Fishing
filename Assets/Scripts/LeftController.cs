2a7ac4c28e99ea89a5796b65f259c7fb34d9752d
Ruaridh Smith <ruaridhsmith@ymail.com>
2016-06-26 22:57:02 +0100

    Lots of work on the reel. Nearly complete. Needs work on the angle between controller and reel.

---
+++ Assets/Scripts/LeftController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeftController : MonoBehaviour {
+    public GameObject controllerManager;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Reel"))
+        {
+            controllerManager.GetComponent<SteamVR_ControllerManager>().setReelTouching(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Reel"))
+        {
+            controllerManager.GetComponent<SteamVR_ControllerManager>().setReelTouching(false);
+        }
+    }
+}
---
+++ Assets/Scripts/ReelController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReelController : MonoBehaviour {
+    public GameObject rod;
+    public GameObject reelHandle;
+
+    private Transform reelHandleTransform;
+
+	// Use this for initialization
+	void Start () {
+        reelHandleTransform = reelHandle.GetComponent<Transform>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //this.transform.position = rod.transform.position + new Vector3(-0.02f, -0.5f, 0f);
+    }
+
+    void FixedUpdate ()
+    {
+        reelHandleTransform.localEulerAngles = new Vector3 (0f, 0f, 0f);
+    }
+}
