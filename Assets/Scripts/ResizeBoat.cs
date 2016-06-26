daac3b1ad1946e5fdbae03ef8c9636fac6011fbd
Keir Smith <kaffse@gmail.com>
2016-06-26 15:59:27 +0100

    Second attempt at dymanic bucket resizing...

--- Assets/Scripts/ResizeBoat.cs
+++ Assets/Scripts/ResizeBoat.cs
@@ -5,12 +5,14 @@ using Valve.VR;
 public class ResizeBoat : MonoBehaviour {
     public GameObject boatBottom;
     public GameObject playArea;
+    public GameObject bucket;
 
     // Use this for initialization
     void Start() {
         if (playArea.activeSelf == true)
         {
             var boatTransform = boatBottom.GetComponent<Transform>();
+            var bucketTransform = bucket.GetComponent<Transform>();
 
             var chaperone = OpenVR.Chaperone;
             float playAreaX = 2.0f;
@@ -22,6 +24,7 @@ public class ResizeBoat : MonoBehaviour {
                 Debug.Log("Error loading Chaperone");
             Debug.Log("X:" + playAreaX.ToString() + " Y:" + playAreaY.ToString());
             boatTransform.localScale = new Vector3(playAreaX * 0.9f, 0.1f, playAreaY * 0.9f);
+            bucketTransform.localPosition = new Vector3(0.4f, 2f, 0.4f);
         }
         else Debug.Log("Play Area not active");
     }
