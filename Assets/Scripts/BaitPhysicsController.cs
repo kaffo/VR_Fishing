31ed5f312278f511b32662759756171d24437973
Keir Smith <kaffse@gmail.com>
2016-07-06 15:27:42 +0100

    Fish moving far more realistically under and over water. Initial break limits for the joints. Temporarly disabled water/boat for testing

--- Assets/Scripts/BaitPhysicsController.cs
+++ Assets/Scripts/BaitPhysicsController.cs
@@ -8,21 +8,33 @@ public class BaitPhysicsController : MonoBehaviour {
     public GameObject rod;
 
     private Rigidbody rb;
+    private SpringJoint rodJoint;
     private SteamVR_ControllerManager conManScript;
     private GameObject fish;
 
-	// Use this for initialization
-	void Start () {
+    private float rodToHook;
+    private float streach;
+
+    // Use this for initialization
+    void Start () {
         rb = GetComponent<Rigidbody>();
+        rodJoint = rod.GetComponent<SpringJoint>();
         conManScript = controllerManager.GetComponent<SteamVR_ControllerManager>();
         fish = null;
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate () {
+	void Update () {
         if (transform.position.y < 0)
         {
             rb.drag = 40;
+            rodToHook = Vector3.Distance(rod.transform.position, transform.position);
+            streach = rodToHook - (rodJoint.maxDistance + 1.0f);
+            if (fish != null && streach > 0)
+            {
+                //Debug.Log("Mag: " + Vector3.Distance(rod.transform.position, transform.position) + " Dist: " + rodJoint.maxDistance);
+                conManScript.Rumble(1, (ushort)(500 * streach));
+            }
         }
         else
         {
@@ -37,8 +49,9 @@ public class BaitPhysicsController : MonoBehaviour {
             fish.transform.eulerAngles = new Vector3(90, 0, 0);
             fish.transform.position = transform.position - new Vector3(0, 0, -0.2f);
             fish.GetComponent<FixedJoint>().connectedBody = rb;
-            fish.GetComponent<FishMovement>().rod = rod;       
-            conManScript.Rumble(1, 500);
+            fish.GetComponent<FishMovement>().rod = rod;
+            fish.GetComponent<FishMovement>().hook = gameObject;
+            conManScript.Rumble(1, 2000);
         }
         if (other.gameObject.CompareTag("Bucket") && fish != null)
         {
--- Assets/Scripts/FishMovement.cs
+++ Assets/Scripts/FishMovement.cs
@@ -2,17 +2,18 @@
 using System.Collections;
 
 public class FishMovement : MonoBehaviour {
-    private Rigidbody rb;
-
     public GameObject rod;
+    public GameObject hook;
 
     //Variables for fish movement
     [Range(0.0f, 10.0f)]
-    public float stamina = 3.0f;
+    public float stamina = 8.0f;
+    [Range(0.0f, 10.0f)]
+    public float staminaMin = 2.0f;
     [Range(0.0f, 1.0f)]
     public float chanceToThrash = 0.5f;
-    [Range(0.0f, 1.0f)]
-    public float aggressiveness = 0.2f;
+    [Range(0.0f, 10.0f)]
+    public float aggressiveness = 3f;
     [Range(0.0f, 1.0f)]
     public float likenessY = 0.3f;
     [Range(0.0f, 1.0f)]
@@ -21,41 +22,65 @@ public class FishMovement : MonoBehaviour {
     public float likenessSides = 0.8f;
 
     private float currentThrashTimer;
+    private Rigidbody rb;
+    private Rigidbody hookrb;
+    private Vector3 fishForce;
 
     // Use this for initialization
     void Start () {
         rb = GetComponent<Rigidbody>();
+        hookrb = hook.GetComponent<Rigidbody>();
         currentThrashTimer = 0;
+        fishForce = Vector3.zero;
+
+        GetComponent<FixedJoint>().breakForce = 10f;
 	}
-	
+
 	// Update is called once per frame
-	void FixedUpdate () {
+	void Update () {
         if (currentThrashTimer > 0)
         {
-            Vector3 fishForce = Vector3.zero;
-            float fishUp = Random.Range(0f, likenessY) * Random.Range(0, aggressiveness);
-            float fishAway = Random.Range(0f, likenessAway) * Random.Range(0f, aggressiveness);
-            float fishSides = Random.Range(0f, likenessSides) * Random.Range(0f, aggressiveness);
-
-            Vector3 upVector = transform.up * fishUp;
-
-            Vector3 awayFromPlayer = transform.position - rod.transform.position;
-            awayFromPlayer.Normalize();
-            awayFromPlayer *= fishAway;
-
-            Vector3 sideVector = Vector3.Cross(awayFromPlayer, transform.up);
-            sideVector *= fishSides;
-
-            rb.AddForce(upVector + awayFromPlayer + sideVector);
-
             currentThrashTimer -= Time.deltaTime;
         }
         else
         {
             if (Random.Range(0f, 1f) < chanceToThrash)
             {
-                currentThrashTimer = Random.Range(0f, stamina);
+                currentThrashTimer = Random.Range(staminaMin, stamina);
+
+                float fishUp = Random.Range(-likenessY, likenessY) * Random.Range(0, aggressiveness);
+                float fishAway = Random.Range(0f, likenessAway) * Random.Range(0f, aggressiveness);
+                float fishSides = Random.Range(-likenessSides, likenessSides) * Random.Range(0f, aggressiveness);
+
+                Vector3 upVector = transform.up * fishUp;
+
+                Vector3 awayFromPlayer = transform.position - rod.transform.position;
+                awayFromPlayer.Normalize();
+                awayFromPlayer *= fishAway;
+
+                Vector3 sideVector = Vector3.Cross(awayFromPlayer, transform.up);
+                sideVector *= fishSides;
+
+                fishForce = upVector + awayFromPlayer + sideVector;
+                Debug.Log("Time: " + currentThrashTimer);
+                Debug.Log("Up: " + upVector + " Away: " + awayFromPlayer + " Sides: " + sideVector + " Final: " + fishForce);
+                Debug.DrawLine(transform.position, transform.position + fishForce, Color.yellow, currentThrashTimer);
             }
         }
 	}
+
+    void FixedUpdate()
+    {
+        if (currentThrashTimer > 0)
+        {
+            if (transform.position.y > 0)
+            {
+                rb.AddForce(fishForce * 0.1f, ForceMode.Impulse);
+            }
+            else
+            {
+                rb.AddForce(fishForce, ForceMode.Impulse);
+            }
+        }
+    }
 }
